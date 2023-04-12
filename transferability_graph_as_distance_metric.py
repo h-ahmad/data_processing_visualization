4a6b6f3ab5d27bacf9c3b6ede2346c3ae891944c
Hussain Ahmad <hamadni@yahoo.com>
2023-04-12 15:40:44 +0200

    Update transferability_graph_as_distance_metric.py

--- transferability_graph_as_distance_metric.py
+++ transferability_graph_as_distance_metric.py
@@ -17,22 +17,26 @@ from statistics import mean
 import random
 
 seed = np.random.RandomState(seed=3)   # seed:  RandomState(MT19937)
-mds = manifold.MDS(n_components=2,max_iter=30,eps=1e-9,random_state=seed,dissimilarity="precomputed",n_jobs=1) # mds:  <class 'sklearn.manifold._mds.MDS'>
+mds = manifold.MDS(n_components=7, max_iter=30, eps=1e-9, random_state=seed,
+                   dissimilarity="precomputed", n_jobs=1)  # mds:  <class 'sklearn.manifold._mds.MDS'>
 
-feature_path =  'PACS_features.pkl'
+feature_path = 'PACS_features.pkl'
 with open(feature_path, 'rb') as file:
     data_store = pickle.load(file)
-    print('data_store: ', data_store.keys()) 
+    print('data_store: ', data_store.keys())
     features = data_store['features']
     print('features: ', features.keys())
     data = features['feats']
     labels = features['labels']
     print('labels: ', labels.keys())
-    
-    
+
     colors = ['red', 'blue', 'darksalmon', 'green', 'black', 'yellow', 'magenta']
+    legen_name = []
+    global_array = []
+    fig, ax = plt.subplots()
     for index, env in enumerate(labels):
         dom_color = colors[index]
+        dom_array = []
         env_labels = np.array(torch.tensor(labels[env]).cpu())
         env_data = np.array(torch.stack(data[env]).cpu())
         # get unique class and their counts.
@@ -44,15 +48,23 @@ with open(feature_path, 'rb') as file:
                 # Class wise sorting.
                 if i == j:
                     cls_data.append(env_data[iteration])
-                    cls_label.append(j)       
-                    
-            cls_array = np.array(cls_data)   # domain-class pair (d, c) --> M = D * C     
+                    cls_label.append(j)
+
+            # domain-class pair (d, c) --> M = D * C
+            cls_array = np.array(cls_data)
             cls_array -= cls_array.mean()
             similarity = euclidean_distances(cls_array)
             similarity = np.average(similarity)
-            similarity = np.array(similarity)
-            
-            # pos = mds.fit(similarity).embedding_
-            area = cls_array.shape[0]
-            plt.scatter(similarity, cls_array.shape[0], color=dom_color, s=area, lw=0, label="Dom 1")
+
+            myList = np.array([similarity, cls_array.shape[0]])
+            dom_array.append(myList)
+        global_array.append(np.array(dom_array))
+
+        # pos = mds.fit(similarity).embedding_
+        volume = global_array[index][:, 1]
+
+        scatter = ax.scatter(global_array[index][:, 0], volume, color=dom_color, s=volume, lw=0, alpha=0.6, label='Domain '+str(index+1))
         
+        for i in range(global_array[index].shape[0]):
+            ax.annotate(str(i), (global_array[index][:, 0][i], volume[i]), ha='center', va='center')
+    legend = ax.legend(loc="upper left", markerscale=0.4)
