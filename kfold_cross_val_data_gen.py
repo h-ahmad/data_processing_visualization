49ae216bcc05ee2662a9403792164002b8e8802e
Hussain Ahmad <hamadni@yahoo.com>
2024-02-27 13:47:00 -0800

    Update kfold_cross_val_data_gen.py

--- kfold_cross_val_data_gen.py
+++ kfold_cross_val_data_gen.py
@@ -1,41 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 22:45:46 2024
+
+@author: hussain
+"""
+
 import os
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 from tqdm import tqdm
+import argparse
+import ast
 
-def metadata_generator(data_path):
-    label_names = {'Basophil' : 'basophil', 'Eosinophil' : 'eosinophil', 'Lymphocyte' : 'lymphocyte', 'Monocyte' : 'monocyte', 'Neutrophil' : 'neutrophil'}
-    img_path, label, msk_path, set0, set1, set2, set3, set4 = [], [], [], [], [], [], [], []
-    imgs_path = os.path.join(data_path, 'Original_imgs')
-    masks_path = os.path.join(data_path, 'GT_masks')
-    class_folders = sorted(os.listdir(imgs_path))
+def get_args_parser():
+    parser = argparse.ArgumentParser('Data indexing', add_help=False)
+    parser.add_argument('--output_csv_path', default='datasets/', type=str)
+    parser.add_argument('--output_csv_name', default='metadata_kf10_raabin.csv', type=str)
+    parser.add_argument('--data_path', default='../../../data/raabindata/Original/', type=str, help=['root image path to extract image and mask names'])
+    parser.add_argument('--img_path', default='data/raabindata/Original/', type=str)
+    parser.add_argument('--mask_path', default='data/raabindata/Ground Truth/', type=str)
+    parser.add_argument('--splits', default=10, type=int, help=["number of splits. i.e., 5, 10 etc."])
+    parser.add_argument('--label_names', type=str, default="{'Basophil' : 'basophil', 'Eosinophil' : 'eosinophil', 'Lymphocyte' : 'lymphocyte', 'Monocyte' : 'monocyte', 'Neutrophil' : 'neutrophil'}", help='label mapping')
+    return parser
+
+def metadata_generator(data_path, img_path, mask_path, label_names, splits):    
+    img_path_list, label, msk_path = [], [], []
+    img_count = []
+    class_folders = sorted(os.listdir(data_path))
     for cl in class_folders:
-        for img in tqdm(os.listdir(os.path.join(imgs_path, cl))):
-            img_full_path = os.path.join(imgs_path, os.path.join(cl, img))
-            mask_full_path = os.path.join(masks_path, os.path.join(cl, img))            
-            img_path.append(img_full_path)
+        for img in tqdm(os.listdir(os.path.join(data_path, cl))):
+            img_full_path = os.path.join(img_path, os.path.join(cl+'/', img))
+            mask_full_path = os.path.join(mask_path, os.path.join(cl+'/', img))       
+            img_path_list.append(img_full_path)   
             label.append(label_names[cl])
             msk_path.append(mask_full_path)
-            set0.append("train")
-            set1.append("train")
-            set2.append("train")
-            set3.append("train")
-            set4.append("train")
+            img_count.append("train")
     metadata = pd.DataFrame()
-    metadata['image'] = img_path
+    metadata['image'] = img_path_list
     metadata['mask'] = msk_path
     metadata['label'] = label
-    metadata['set0'] = set0
-    metadata['set1'] = set1
-    metadata['set2'] = set2
-    metadata['set3'] = set3
-    metadata['set4'] = set4 
+    for splt in range(splits):
+        metadata['set'+str(splt)] = img_count
 
     return metadata
 
-def metadata_5split(metadata):
+def metadata_split(metadata, splits):
     seed=314 #for random variable
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+    skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=seed)
     i=0
     x_train={}
     x_test={}
@@ -46,17 +57,15 @@ def metadata_5split(metadata):
         y_train[i], y_test[i] = metadata.label[train_index], metadata.label[test_index]
         i+=1
         
-    metadata.loc[x_test[0], 'set0']='test'
-    metadata.loc[x_test[1], 'set1']='test'
-    metadata.loc[x_test[2], 'set2']='test'
-    metadata.loc[x_test[3], 'set3']='test'
-    metadata.loc[x_test[4], 'set4']='test'
+    for splt in range(splits):
+        metadata.loc[x_test[splt], 'set'+str(splt)]='test'
     return metadata
 
 if __name__ == "__main__":
-    data_path = '../../../datasets/raabindata'
-    metadata = metadata_generator(data_path)    
-    #splitting in 5 test/train-sets
-    metadata = metadata_5split(metadata)   
-    metadata.to_csv("dataset/metadata_kf5.csv", index=False)
-    
+    parser = argparse.ArgumentParser('Data indexing', parents=[get_args_parser()])
+    args = parser.parse_args()
+    args.label_names = ast.literal_eval(args.label_names)
+    metadata = metadata_generator(args.data_path, args.img_path, args.mask_path, args.label_names, args.splits)   
+    metadata = metadata_split(metadata, args.splits) 
+    os.makedirs(args.output_csv_path, exist_ok = True)
+    metadata.to_csv(os.path.join(args.output_csv_path, args.output_csv_name), index=False)
